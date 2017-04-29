15fcb503b79681d9e0768f15f9c2e41b25d01c5e
Konstantin Baierer <unixprog@gmail.com>
2017-04-29 20:11:57 +0200

    polyline and loadimage

--- App.vue
+++ App.vue
@@ -21,6 +21,8 @@
           img(src="./assets/ellipsis.svg")
         button.btn.btn-default(title="Circle",@click="drawShape('Circle')")
           img(src="./assets/circle.svg")
+        button.btn.btn-default(title="Polyline",@click="drawShape('Polyline')")
+          img(src="./assets/polyline.svg")
       .input-group.btn-group
         span.input-group-addon.hidden-xs.hidden-sm Action
         button.btn.btn-default(@click="setMode('Modify')",title="Select")
@@ -35,20 +37,21 @@
           img(src="./assets/zoom-out.svg")
         span.dropdown
           button.btn.btn-default.dropdown-toggle(type='button', data-toggle='dropdown')
-            | {{ zoomValue * 100 }} %
+            | {{ parseInt(zoomValue * 100) }} %
             span.caret
           ul.dropdown-menu
             li
-              a(href='#') 100 %
+              a(href='#',@click="zoom(1)") 100 %
             li
-              a(href='#') Fit to canvas
+              a(href='#',@click="zoom('fit')") Fit to canvas
             li
-              a(href='#') Fit to width
-  .panel-body
-    div(
-      ref="canvas",
-      v-bind:style="`width: ${width}; height: ${height}`"
-    )
+              a(href='#',@click="zoom('width')") Fit to width
+            li
+              a(href='#',@click="zoom('width')") Fit to height
+  div(
+    ref="canvas",
+    v-bind:style="`width: ${width}; height: ${height}`"
+  )
 </template>
 
 <script>
@@ -84,40 +87,50 @@ export default {
       {value: 'Select', text: 'Select'},
     ]}},
     style: {type: Object, default() { return {
-      strokeWidth: 1,
       strokeColor: '#3B3BFF',
       fillColor: '#3B3BFF',
+      strokeWidth: 2,
       fillOpacity: 0.4,
       hoverable: {
-        strokeWidth: 1,
         strokeColor: '#00ff00',
         fillColor: '#00ff00',
+        strokeWidth: 3,
         fillOpacity: 0.4,
       },
       creatable: {
-        strokeWidth: 1,
         strokeColor: '#ff0000',
         fillColor: '#ff0000',
+        strokeWidth: 3,
         fillOpacity: 0.4,
       },
       selectable: {
         strokeColor: '#ff00ff',
         fillColor: '#ff00ff',
+        strokeWidth: 3,
+        fillOpacity: 0.4,
       },
     }}},
   },
   mounted() {
     this.image = new xrx.drawing.Drawing(this.$refs.canvas)
-    this.image.eventShapeModify = () => this.applyStyles()
-    this.image.eventViewboxChange = () => this.applyStyles()
-    this.image.setBackgroundImage(this.initialImage, () => {
-      this.image.getViewbox().fitToWidth(false)
-      this.image.getViewbox().setZoomFactorMax(this.zoomFactorMax)
-      this.image.draw()
-    })
+    // this.image.eventShapeModify = () => this.applyStyles()
+    this.image.eventShapeCreated = () => {
+      this.setMode('HoverMult')
+      this.applyStyles()
+    }
+    // this.image.eventViewboxChange = () => this.applyStyles()
+    this.loadImage(this.initialImage)
   },
   methods: {
     dump(...args) { console.log(args) },
+    loadImage(imgSrc) {
+      this.image.setBackgroundImage(imgSrc, () => {
+        this.image.getViewbox().fit(false)
+        this.image.getViewbox().setZoomFactorMax(this.zoomFactorMax)
+        this.zoomValue = this.image.getViewbox().getZoomValue()
+        this.image.draw()
+      })
+    },
     setMode(mode, ...args) {
       if (mode === 'HoverMult') this.image.setModeHover(true);
       else this.image[`setMode${mode}`](...args);
@@ -126,12 +139,16 @@ export default {
     zoom(amount) {
       if (amount === 'in') this.image.getViewbox().zoomIn()
       else if (amount === 'out') this.image.getViewbox().zoomOut()
-      else this.image.getViewbox().setZoomValue(amount)
+      else if (amount === 'fit') this.image.getViewbox().fit(true)
+      else if (amount === 'width') this.image.getViewbox().fitToWidth(true)
+      else if (amount === 'height') this.image.getViewbox().fitToHeight(true)
+      else this.image.getViewbox().zoomTo(amount)
       this.zoomValue = this.image.getViewbox().getZoomValue()
       console.log(this.zoomValue)
       this.image.draw()
     },
     applyStyles() {
+      console.log('yello')
       this.image.getLayerShape().getShapes().forEach(shape => {
         XrxUtils.applyStyle(shape, this.style)
       })
@@ -140,7 +157,7 @@ export default {
       const shape = new xrx.shape[shapeName](this.image)
       XrxUtils.applyStyle(shape, this.style)
       this.setMode('Create', shape.getCreatable())
-      this.image.getLayerShape().addShapes(shape)
+      // this.image.getLayerShape().addShapes(shape)
     },
     removeShape() {
       if (this.image.getSelectedShape() === undefined) {
