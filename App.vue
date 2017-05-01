80ffeb555a6142f2d5ea5720bf94d14cc9cd3f51
Konstantin Baierer <unixprog@gmail.com>
2017-05-01 03:48:19 +0200

    css, styling, distinguish select/modify

--- App.vue
+++ App.vue
@@ -2,6 +2,15 @@
 .panel.panel-default
   .panel-heading
     .form-inline.xrx-toolbar
+      .input-group
+        span.input-group-addon.hidden-sm.hidden-xs Mode
+        select.form-control(@click="setMode($event.target.value)")
+          option(
+            v-for="value in modesEnabled",
+            v-bind:value="value",
+            v-bind:disabled="modesAvailable.find(x => x.value == value).disabled"
+            v-bind:selected="value == mode"
+          ) {{ modesAvailable.find(x => x.value == value).text }}
       .input-group.btn-group
         span.input-group-addon.hidden-sm.hidden-xs Shape
         button.btn.btn-default(title="Polygon",@click="drawShape('Polygon')")
@@ -17,26 +26,21 @@
         button.btn.btn-default(title="line",@click="drawShape('Line')")
           img(src="./assets/line.svg")
       .input-group
-        span.input-group-addon.hidden-sm.hidden-xs Mode
-        select.form-control(@click="setMode($event.target.value)")
-          option(
-            v-for="value in modesEnabled",
-            v-bind:value="value",
-            v-bind:disabled="modesAvailable.find(x => x.value == value).disabled"
-            v-bind:selected="value == mode"
-          ) {{ modesAvailable.find(x => x.value == value).text }}
-      .input-group
-        span.input-group-addon.hidden-sm.hidden-xs SVG
+        span.input-group-addon.hidden-sm.hidden-xs File
         button.btn.btn-default(title="Save SVG",@click="showImexport('export')")
           img(src="./assets/save.svg")
-        button.btn.btn-default(title="Save SVG",@click="showImexport('import')")
+        button.btn.btn-default(title="Load SVG",@click="showImexport('import')")
           img(src="./assets/upload.svg")
+        button.btn.btn-default(title="Background Image",@click="showImageModal")
+          img(src="./assets/image.svg")
       .input-group.btn-group
         span.input-group-addon.hidden-xs.hidden-sm Action
-        button.btn.btn-default(@click="setMode('Modify')",title="Select")
+        button.btn.btn-default(@click="setMode('Modify')",title="Modify")
+          img(src="./assets/hand.svg")
+        button.btn.btn-default(@click="setMode('Select')",title="Select")
           img(src="./assets/cursor.svg")
-        button.btn.btn-default(title="Delete",@click="removeShape")
-          img(src="./assets/delete.svg")
+        button.btn.btn-default(title="Delete",@click="removeSelected")
+          img(src="./assets/remove.svg")
       .input-group.btn-group
         span.input-group-addon.hidden-sm.hidden-xs Zoom
         button.btn.btn-default(title="Zoom in",@click="zoom('in')")
@@ -56,19 +60,31 @@
               a(href='#',@click="zoom('width')") Fit to width
             li
               a(href='#',@click="zoom('width')") Fit to height
+
+  // Canvas
   div(
     ref="canvas",
     v-bind:style="`width: ${width}; height: ${height}`"
   )
-  // Modal
-  .svg-panel.modal.fade(role='dialog', ref="svgPanel")
+
+  // SVG Import/Export Modal
+  .imexport-modal.modal.fade(role='dialog', ref="imexportModal", tabindex=-1)
     .modal-dialog
-      // Modal content
       .modal-content
-        .modal-header
+        .modal-header(v-if='imexport == "import"') Import from SVG
+        .modal-header(v-else) Export as SVG
         .modal-body
-          textarea(v-model="svgImExPort", placeholder="SVG here")
-          button.btn.btn-success(@click="loadSvg",v-show="imexport == 'import'") Load
+          textarea.form-control(v-model="svgImExPort", placeholder="SVG here")
+          button.form-control.btn.btn-success(@click="loadSvg",v-show="imexport == 'import'") Load
+
+  // Background image modal
+  .image-modal.modal.fade(role='dialog', ref="imageModal", tabindex=-1)
+    .modal-dialog
+      .modal-content
+        .modal-header Set background image
+        .modal-body
+          input.form-control(format='url', v-model="backgroundImage", placeholder="Image URL")
+          button.form-control.btn.btn-success(@click="loadImage") Set background image
 </template>
 
 <script>
@@ -140,13 +156,14 @@ export default {
     this.image.eventShapeSelected = (shape) =>{
       console.log("selected", shape)
     }
+    this.backgroundImage = this.initialImage
     // this.image.eventViewboxChange = () => this.applyStyles()
-    this.loadImage(this.initialImage)
+    this.loadImage()
   },
   methods: {
     dump(...args) { console.log(args) },
-    loadImage(imgSrc) {
-      this.image.setBackgroundImage(imgSrc, () => {
+    loadImage() {
+      this.image.setBackgroundImage(this.backgroundImage, () => {
         this.image.getViewbox().fit(false)
         this.image.getViewbox().setZoomFactorMax(this.zoomFactorMax)
         this.zoomValue = this.image.getViewbox().getZoomValue()
@@ -181,8 +198,8 @@ export default {
       this.setMode('Create', shape.getCreatable())
       // this.image.getLayerShape().addShapes(shape)
     },
-    removeShape() {
-      if (this.image.getSelectedShape()) {
+    removeSelected() {
+      if (!this.image.getSelectedShape()) {
         window.alert("Please select a shape")
       } else {
         if (window.confirm("Delete selected shape?")) {
@@ -195,40 +212,48 @@ export default {
       this.svgImExPort = (this.imexport === 'import')  
         ? '' 
         : XrxUtils.svgFromDrawing(this.image)
-      $(this.$refs.svgPanel).modal('show')
+      $(this.$refs.imexportModal).modal('show')
     },
     loadSvg() {
       this.image.getLayerShape().removeShapes();
       XrxUtils.drawFromSvg(this.svgImExPort, this.image)
       this.applyStyles()
-      $(this.$refs.svgPanel).modal('hide')
+      $(this.$refs.imexportModal).modal('hide')
+    },
+    showImageModal() {
+      $(this.$refs.imageModal).modal('show')
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-.btn {
-  padding-left: 6px;
-  padding-right: 6px;
-}
 .panel {
-  background: transparent
+  background: transparent;
+  .panel-heading {
+    padding: 0;
+  }
+  /* .xrx-toolbar .input-group { display: inline-table !important }*/
+  .xrx-toolbar {
+    .input-group,
+    select,
+    .dropdown-toggle,
+    button
+    {
+      height: 32px;
+      & > img {
+        height: 20px;
+      }
+      &.btn {
+        padding-left: 6px;
+        padding-right: 6px;
+      }
+    }
+  }
 }
-/* .xrx-toolbar .input-group { display: inline-table !important }*/
-.xrx-toolbar .input-group,
-.xrx-toolbar select,
-.xrx-toolbar .dropdown-toggle
-{
-  height: 37px;
-}
-button > img {
-  height: 26px;
-}
-.svg-panel {
+.imexport-modal {
   textarea {
     font-family: monospace;
-    width: 90%;
     min-height: 200px;
   }
 }
