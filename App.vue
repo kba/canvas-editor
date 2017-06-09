e064a0a21ef57236b9c543b52c2d85bff34b1693
Konstantin Baierer <baierer@ub.uni-heidelberg.de>
2017-06-09 16:07:06 +0200

    include base64-encoded default image

--- App.vue
+++ App.vue
@@ -232,7 +232,7 @@ export default {
      * #### `initial-image`
      * Initial background image to load.
      */
-    initialImage: {type: String, default: 'assets/1024px-Ghostscript_Tiger.svg.png', required: true},
+    initialImage: {type: String, default: require('./assets/1024px-Ghostscript_Tiger.svg.png'), required: true},
 
     /**
      * 
